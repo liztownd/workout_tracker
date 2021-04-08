5853ee6efee3c5f91bca6e3ad1ebf2d90ed5eadc
Liz Town <liztown@gmail.com>
2021-04-07 21:35:05 -0500

    adjusting index to pull the module export from workout and then export for use

--- models/index.js
+++ models/index.js
@@ -1,11 +1,3 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const WorkoutSchema = new Schema({
-  title: String,
-  body: String
-});
-
-const Workout = mongoose.model("Workout", WorkoutSchema);
-
-module.exports = Workout;
+module.exports = {
+    Workout: require('./workout')
+};
