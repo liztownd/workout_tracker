a666ba2e1c5094ff91f568e61ef46977ca895bcd
Liz Town <liztown@gmail.com>
2021-04-11 16:28:40 -0500

    splitting routes into separate folder

---
+++ routes/apiRoutes.js
@@ -0,0 +1,67 @@
+const db = require("../models");
+
+module.exports = (app) => {
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.aggregate([
+            {
+                $addFields: {
+                    totalDuration: {
+                        $sum: '$exercises.duration'
+                    }
+                }
+            }
+        ])
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+    
+    app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.findByIdAndUpdate(req.params.id,
+            {
+                $push: {
+                    exercises: req.body
+                }
+            })
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+    
+    app.post("/api/workouts", (req, res) => {
+        console.log(req.body);
+        db.Workout.create(req.body)
+            .then(dbWorkout => {
+                console.log(dbWorkout);
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+    
+    app.get('/api/workouts/range', (req, res) => {
+        db.Workout.aggregate([
+            {
+                $addFields: {
+                    totalDuration: {
+                        $sum: '$exercises.duration'
+                    }
+                }
+            }
+        ])
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+    
+};
--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const path = require("path");
 
-const PORT = process.env.PORT || 3000;
-
-const db = require("./models");
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -23,79 +20,9 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useFindAndModify: false
 });
 
-//api routes
-app.get("/api/workouts", (req, res) => {
-    db.Workout.aggregate([
-        {
-            $addFields: {
-                totalDuration: {
-                    $sum: '$exercises.duration'
-                }
-            }
-        }
-    ])
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-});
 
-app.put("/api/workouts/:id", (req, res) => {
-    db.Workout.findByIdAndUpdate(req.params.id,
-        {
-            $push: {
-                exercises: req.body
-            }
-        })
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-});
-
-app.post("/api/workouts", (req, res) => {
-    console.log(req.body);
-    db.Workout.create(req.body)
-        .then(dbWorkout => {
-            console.log(dbWorkout);
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-});
-
-app.get('/api/workouts/range', (req, res) => {
-    db.Workout.aggregate([
-        {
-            $addFields: {
-                totalDuration: {
-                    $sum: '$exercises.duration'
-                }
-            }
-        }
-    ])
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-});
-
-//html routes
-app.get('/exercise', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/exercise.html'))
-});
-
-app.get('/stats', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/stats.html'))
-});
-
+require('./routes/apiRoutes.js')(app);
+require('./routes/htmlRoutes.js')(app);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
